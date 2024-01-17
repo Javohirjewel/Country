c301a8a20566ef3ef00245575ae440cb1ae7e8a8
umidshax71 <ismoilovumid609@gmail.com>
2024-01-17 16:15:11 +0500

    update function

--- js/main.js
+++ js/main.js
@@ -1,33 +1,38 @@
 const input = document.querySelector(".input")
+// const regionSelect = document.querySelector('.select')
+// console.dir(regionSelect)
+const wrapper = document.querySelector('.wrapper')
 
-const ok = () =>{
-   input.addEventListener('change', () => {
-    console.log(input.value.toLowerCase());
-   }) 
+const drop = document.querySelector('.drop')
+const  options = document.querySelector('.options')
+
+function showModal() {
+    document.querySelector(".options").classList.toggle("show");
 }
-ok()
+
+
 
 const LoadingApi = () =>{
     fetch('https://restcountries.com/v3.1/all')
     .then(response => response.json())
     .then(data => countriesHtml(data))
 }
-LoadingApi()
-
-const countriesHtml = countries =>{
-    const countriesHtml = countries.map(country => getCountry(country))
-    const wrapper = document.querySelector('.wrapper')
-    wrapper.innerHTML = countriesHtml;
-}
-
+LoadingApi()    
 const getCountry = (country) =>{
-    // console.log(country)
-    return `
-    <div class='item'>
-    <div class='item__img'>
-        <img src=${country.flags.png} alt = 'salom' >
-    </div>
-    <div class='item__wrapper'>
+    options.addEventListener('click', (e) =>{
+        drop.innerHTML = e.target.textContent
+            if (e.target.textContent == country.region){
+                
+            }
+            showModal()
+     
+        })
+            return `
+            <div class='item'>
+            <div class='item__img'>
+                <img src=${country.flags.png} alt = 'salom' >
+            </div>
+            <div class='item__wrapper'>
             <div class='item__title'>
                 ${country.name.common}
             </div>
@@ -41,6 +46,71 @@ const getCountry = (country) =>{
                 <span>Capital:</span> ${country.capital ? country.capital : `no found`}
             </div>
             </div>
-        </div> 
-    `
+            </div>`
+} 
+const countriesHtml = countries =>{
+    const countriesHtml = countries.map(country => getCountry(country))
+    wrapper.innerHTML = countriesHtml;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const searchInput = () =>{
+    input.addEventListener('keypress', (e) => {
+        if (e.keyCode === 13 && input.value.length > 0 ) {
+            const LoadingApi = () =>{
+                fetch('https://restcountries.com/v3.1/all')
+                .then(response => response.json())
+                .then(data => countriesHtml(data))
+            }
+            LoadingApi()    
+            const getCountry = (country) =>{
+                if(input.value.toLowerCase() == country.name.common.toLowerCase()){
+                    return `
+                    <div class='item'>
+                    <div class='item__img'>
+                        <img src=${country.flags.png} alt = 'salom' >
+                    </div>
+                    <div class='item__wrapper'>
+                        <div class='item__title'>
+                            ${country.name.common}
+                        </div>
+                        <div class="item__popul">
+                            <span>Population:</span> ${country.population ? (country.population) : `not found`}
+                        </div>
+                        <div class="item__region">
+                            <span>Region:</span> ${country.region}
+                        </div>
+                        <div class="item__capital">
+                            <span>Capital:</span> ${country.capital ? country.capital : `no found`}
+                        </div>
+                        </div>
+                    </div>
+                    `
+                    
+                }
+            } 
+            const countriesHtml = countries =>{
+                const countriesHtml = countries.map(country => getCountry(country))
+                wrapper.innerHTML = countriesHtml;
+            }
+            
+        }
+    })
+
+}
+searchInput()
+
