22bb057da4d468eb959e4a6da061c64c55a81469
umidshax71 <ismoilovumid609@gmail.com>
2024-01-20 18:12:50 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 const input = document.querySelector(".input")
-// const regionSelect = document.querySelector('.select')
-// console.dir(regionSelect)
 const wrapper = document.querySelector('.wrapper')
-
 const drop = document.querySelector('.drop')
 const  options = document.querySelector('.options')
 
@@ -10,107 +7,70 @@ function showModal() {
     document.querySelector(".options").classList.toggle("show");
 }
 
-
-
-const LoadingApi = () =>{
-    fetch('https://restcountries.com/v3.1/all')
-    .then(response => response.json())
-    .then(data => countriesHtml(data))
-}
-LoadingApi()    
-const getCountry = (country) =>{
-    options.addEventListener('click', (e) =>{
-        drop.innerHTML = e.target.textContent
-            if (e.target.textContent == country.region){
-                
-            }
-            showModal()
-     
-        })
-            return `
-            <div class='item'>
+const inner = (data) =>{
+    let arr = data?.map(el => {
+        console.log(el)
+        const {flags, name, population, region, capital} = el
+    
+        return `<div class='item'>
             <div class='item__img'>
-                <img src=${country.flags.png} alt = 'salom' >
+                <img src=${flags.png} alt = 'salom' >
             </div>
             <div class='item__wrapper'>
             <div class='item__title'>
-                ${country.name.common}
+                ${name.common}
             </div>
             <div class="item__popul">
-                <span>Population:</span> ${country.population ? (country.population) : `not found`}
+                <span>Population:</span> ${population ? (population) : `not found`}
             </div>
             <div class="item__region">
-                <span>Region:</span> ${country.region}
+                <span>Region:</span> ${region}
             </div>
             <div class="item__capital">
-                <span>Capital:</span> ${country.capital ? country.capital : `no found`}
+                <span>Capital:</span> ${capital ? capital : `no found`}
             </div>
             </div>
             </div>`
-} 
-const countriesHtml = countries =>{
-    const countriesHtml = countries.map(country => getCountry(country))
-    wrapper.innerHTML = countriesHtml;
+        
+    })
+
+    console.log(arr)
+    wrapper.innerHTML = arr
+    
+
+}    
+
+
+const LoadingApi = () =>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(response => response.json())
+        .then(data => searchInput(data))
 }
+LoadingApi()   
 
+input.addEventListener('input', () => {
+    LoadingApi()  
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const searchInput = () =>{
-    input.addEventListener('keypress', (e) => {
-        if (e.keyCode === 13 && input.value.length > 0 ) {
-            const LoadingApi = () =>{
-                fetch('https://restcountries.com/v3.1/all')
-                .then(response => response.json())
-                .then(data => countriesHtml(data))
-            }
-            LoadingApi()    
-            const getCountry = (country) =>{
-                if(input.value.toLowerCase() == country.name.common.toLowerCase()){
-                    return `
-                    <div class='item'>
-                    <div class='item__img'>
-                        <img src=${country.flags.png} alt = 'salom' >
-                    </div>
-                    <div class='item__wrapper'>
-                        <div class='item__title'>
-                            ${country.name.common}
-                        </div>
-                        <div class="item__popul">
-                            <span>Population:</span> ${country.population ? (country.population) : `not found`}
-                        </div>
-                        <div class="item__region">
-                            <span>Region:</span> ${country.region}
-                        </div>
-                        <div class="item__capital">
-                            <span>Capital:</span> ${country.capital ? country.capital : `no found`}
-                        </div>
-                        </div>
-                    </div>
-                    `
-                    
-                }
-            } 
-            const countriesHtml = countries =>{
-                const countriesHtml = countries.map(country => getCountry(country))
-                wrapper.innerHTML = countriesHtml;
-            }
-            
+const searchInput = (country) =>{
+    let newData = country?.filter((item) => {
+        if(input.value.length > 0){
+           return input.value.toLowerCase().includes(item?.name?.common.toLowerCase())
+        }else{
+            return country
         }
     })
 
+    inner(newData)
 }
-searchInput()
 
+
+// const getCountry = (country) =>{
+//     //     options.addEventListener('click', (e) =>{
+            
+//     //         drop.innerHTML = e.target.textContent
+//     //           
+                
+//     //     })
+                       
+//     // } 
